8a92ad62235c9eb250a71ec37c165966390d68fd
Nambi005 <nambiar.sneha123@gmail.com>
2026-03-23 00:25:36 +1100

    Create SmartCard.java

---
+++ src/main/java/SmartCard.java
@@ -0,0 +1,36 @@
+public class SmartCard {
+    private int cardID;
+    private double balance;
+
+    public void setSmartCardID(int cardID) {
+        this.cardID = cardID;
+    }
+
+    public int getSmartCardID() {
+        return cardID;
+    }
+
+    public void setBalance(double balance) {
+        if (balance >= 5) {
+            this.balance = balance;
+        } else {
+            System.out.println("Minimum balance is $5");
+        }
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void topUp(double amount) {
+        balance += amount;
+    }
+
+    public boolean deductFare(double fare) {
+        if (balance >= fare) {
+            balance -= fare;
+            return true;
+        }
+        return false;
+    }
+}
